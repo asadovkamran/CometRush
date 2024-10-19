fd5ea625d289f035e7b1a1e12caa6578b5025da0
nutellaj <askmr92@gmail.com>
2024-10-19 17:21:56 +0400

    bugfix explosions

---
+++ Assets/Scripts/HitDetection.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetection : MonoBehaviour
+{
+    public LayerMask layer;
+    public GameObject[] explosions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layer))
+            {
+                Debug.Log(hit.transform.name);
+                Debug.Log("hit");
+                Destroy(hit.transform.gameObject);
+                Instantiate(explosions[Random.Range(0, explosions.Length)], hit.transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
