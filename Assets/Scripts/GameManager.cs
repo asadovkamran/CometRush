33925aa88ed7553c0d4610bfcaef58c24966eebc
nutellaj <askmr92@gmail.com>
2024-10-22 16:26:37 +0400

    added shields, shields ui, shields reduction on impact

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public GameConstants GAME_CONSTANTS;
     public int score = 0;
+    [SerializeField] private float shieldsCapacity;
+
+    public event Action<float> OnShieldsUpdated;
 
     private void Awake()
     {
@@ -15,7 +21,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        shieldsCapacity = GAME_CONSTANTS.PLAYER_SHIELDS_CAPACITY;
     }
 
     // Update is called once per frame
@@ -25,5 +31,23 @@ public class GameManager : MonoBehaviour
         {
             Application.Quit();
         }
+
+        if (shieldsCapacity <= 0) {
+            HandleGameOver();
+        }
+    }
+
+    public void UpdateShields(float amount)
+    {
+        shieldsCapacity += amount;
+        OnShieldsUpdated?.Invoke(shieldsCapacity);
+    }
+
+    public float GetShields() { return shieldsCapacity; }
+
+    void HandleGameOver()
+    {
+        Debug.Log("Game over");
+        Time.timeScale = 0;
     }
 }
