f7c1370e74d8c7e418b2099097df56ef3b993db1
nutellaj <askmr92@gmail.com>
2024-10-24 21:49:35 +0400

    created object pool for spawnable prefabs

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -7,13 +7,20 @@ public class Star : MonoBehaviour
 {
     public float speed;
     public GameConstants GAME_CONSTANTS;
+    public ObjectPool pool;
 
     Rigidbody rb;
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        pool = GameObject.Find("StarsPool").GetComponent<ObjectPool>();
 
+        SetInitialPosition();
+    }
+
+    void SetInitialPosition()
+    {
         float spawnDistance = Random.Range(GAME_CONSTANTS.STAR_MIN_SPAWN_DISTANCE, GAME_CONSTANTS.STAR_MAX_SPAWN_DISTANCE);
 
         Vector3 randomSpawnPosition = new Vector3(Random.Range(0, Camera.main.pixelWidth),
@@ -22,7 +29,12 @@ public class Star : MonoBehaviour
 
 
         rb.position = worldPosition;
+    }
 
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        SetInitialPosition();
     }
 
     // Update is called once per frame
@@ -33,7 +45,7 @@ public class Star : MonoBehaviour
 
         if (rb.position.z > Camera.main.transform.position.z)
         {
-            Destroy(gameObject);
+            pool.ReturnToPool(gameObject);
         }
     }
 }
