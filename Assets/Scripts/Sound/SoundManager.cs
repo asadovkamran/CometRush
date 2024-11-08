3e8810eb34d3b77dbee3b37cce09fd4efe06f676
nutellaj <askmr92@gmail.com>
2024-11-08 21:57:42 +0400

    bugfix explosions sound clipping

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -1,13 +1,34 @@
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    private AudioSource _explosionSoundAS;
+    [SerializeField] private AudioClip _explosionSoundClip;
+
+    // when player hits comets in a very short time range
+    // explosion sounds get clipped because they are played in one audio source
+    // to fix this we add extra audio sources and every time we play explosion sound
+    // we do it on the audio source that is free i.e. doesnt play anything
+    private List<AudioSource> _bufferAudioSources = new List<AudioSource>(); // list to keep extra audio sources
+
+    // index to iterate through the list of audio sources
+    // not sure why but iterating for foreach and checking isPlaying didn't work 
+    // so i iterate manually (see HandleCometHitMethod)
+    private int _currentSourceIndex; 
     private AudioSource _cometImpactAS;
+    
     private void Awake()
     {
-        _explosionSoundAS = transform.Find("ExplosionSound").GetComponent<AudioSource>();
         _cometImpactAS = transform.Find("CometImpactSound").GetComponent<AudioSource>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+            source.playOnAwake = false;
+            source.clip = _explosionSoundClip;
+            _bufferAudioSources.Add(source);
+        }
     }
     private void OnEnable()
     {
@@ -23,7 +44,16 @@ public class SoundManager : MonoBehaviour
 
     private void HandleOnCometHit(GameObject obj)
     {
-        _explosionSoundAS.Play();
+        if (_bufferAudioSources.Count == 0) return;
+
+        AudioSource source = _bufferAudioSources[_currentSourceIndex];
+
+        if (source != null && !source.isPlaying)
+        {
+            source.Play();
+
+            _currentSourceIndex = (_currentSourceIndex + 1) % _bufferAudioSources.Count;
+        }
     }
 
     private void HandleOnCometReachPlayer(float damage)
